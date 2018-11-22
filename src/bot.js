77bb0d9a344579f1146cc8f719245a13bdcd98aa
yuhot <41821662+yuhot@users.noreply.github.com>
2018-11-22 22:21:04 +0700

    Add files via upload

--- src/bot.js
+++ src/bot.js
@@ -1,8 +1,28 @@
 const LineConnect = require('./connect');
-let line = require('./main.js');
-let LINE = new line();
+const LINE = require('./main.js');
+console.info("\n\
+   .::::                                 .::          .::      .::\n\
+ .:    .::                               .::           .::   .::  \n\
+.::           .::       .::       .::    .::   .::      .:: .::   \n\
+.::         .::  .::  .::  .::  .::  .:: .:: .:   .::     .::     \n\
+.::   .::::.::    .::.::    .::.::   .:: .::.::::: .::  .:: .::   \n\
+ .::    .:  .::  .::  .::  .::  .::  .:: .::.:         .::   .::  \n\
+  .:::::      .::       .::         .:: .:::  .::::   .::      .::\n\
+                                 .::                              \n");
+console.info("\n\
+=========================================\n\
+BotName: LINE THIRD JS\n\
+Version: FORKED VERSION\n\
+Thanks to @Alfathdirk @TCR_TEAM\n\
+=========================================\n\
+\nNOTE : This bot is made by @Alfathdirk @TCR_TEAM and has been forked by @GoogleX !\n\
+***Copyright belongs to the author***");
 
-
+/*
+| This constant is for auth/login
+| 
+| Change it to your authToken / your email & password
+*/
 const auth = {
 	authToken: '',
 	certificate: '',
@@ -14,7 +34,6 @@ let client =  new LineConnect();
 //let client =  new LineConnect(auth);
 
 client.startx().then(async (res) => {
-	
 	while(true) {
 		try {
 			ops = await client.fetchOps(res.operation.revision);
@@ -27,6 +46,6 @@ client.startx().then(async (res) => {
 				LINE.poll(ops[op])
 			}
 		}
+		//LINE.aLike() //AutoLike (CAUSE LAG)
 	}
 });
-
